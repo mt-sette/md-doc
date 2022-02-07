2548bb162629307a4c6d602d2f065d04640f6301
mt-sette <matteo.sette@digitalcareerinstitute.org>
2022-02-07 11:49:31 +0100

    init

---
+++ src/pages/my-react-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+const MyReactPage = () => {
+    return (
+        <Layout>
+            <h1>My React Page </h1>
+            <p>This is a React Page</p>
+        </Layout>
+    );
+};
+
+export default MyReactPage;
